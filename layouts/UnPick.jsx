b7588f78cb78f84ce3b486fa0fae18fab8f6ca8c
snnehir <snnehir21@gmail.com>
2022-12-28 21:37:58 +0300

    open camera + select from gallery actions

--- layouts/UnPick.jsx
+++ layouts/UnPick.jsx
@@ -1,21 +1,80 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ImageBackground,
+} from 'react-native';
 import styles from '../styles/Style';
+import ImagePicker from 'react-native-image-crop-picker';
+
 
 const UnPick = ({pick}) => {
-    return (
-        <View style={{ alignItems: "center" }}>
-            <Text style={styles.t4}>
-                Take a photo of your plant or select it from your gallery.
-            </Text>
-            <TouchableOpacity style={styles.button}>
-                <Text style={styles.bt1}> Open Camera </Text>
-            </TouchableOpacity>
+  const [image, setImage] = useState(null);
+  const [isImageSelected, setImageSelected] = useState(false);
 
-            <TouchableOpacity onPress={pick} style={styles.button}>
-                <Text style={styles.bt1}> Select from Gallery </Text>
-            </TouchableOpacity>
-        </View>
-    );
+  const openCamera = () => {
+    ImagePicker.openCamera({
+      width: 300,
+      height: 400,
+      cropping: true,
+    }).then(image => {
+      console.log(image);
+      setImage(image.path);
+      setImageSelected(true);
+      //this.bs.current.snapTo(1);
+    });
+  };
+
+  const openGallery = () => {
+    ImagePicker.openPicker({
+      width: 300,
+      height: 400,
+      cropping: true,
+    }).then(image => {
+      console.log(image);
+      setImage(image.path);
+      setImageSelected(true);
+    });
+  };
+
+  return (
+    <View style={{alignItems: 'center'}}>
+      <Text style={styles.t4}>
+        Take a photo of your plant or select it from your gallery.
+      </Text>
+      <View>
+        {isImageSelected && (
+          <ImageBackground
+            source={{
+              uri: image,
+            }}
+            style={{height: 200, width: 200}}
+            imageStyle={{borderRadius: 15}}>
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}></View>
+          </ImageBackground>
+        )}
+
+        <TouchableOpacity
+          onPress={openCamera}
+          title="Open Camera"
+          style={styles.button}>
+          <Text style={styles.bt1}> Open Camera </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={openGallery}
+          title="Open Gallery"
+          style={styles.button}>
+          <Text style={styles.bt1}> Select from Gallery </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 };
 
 export default UnPick;
--- pages/Camera.jsx
+++ pages/Camera.jsx
@@ -1,9 +1,10 @@
-import { View, Text, Image, TouchableOpacity, Button } from "react-native"
+import { View, Text} from "react-native"
 import LinearGradient from "react-native-linear-gradient";
 import styles from "../styles/Style";
 import React, { useState } from 'react';
 import Pick from "../layouts/Pick";
 import UnPick from "../layouts/UnPick";
+import {Picker} from '@react-native-picker/picker';
 
 const Camera = () => {
     const [isAuthenticated, setAuthenticated] = useState(true)
@@ -11,30 +12,35 @@ const Camera = () => {
     const handleUnPick = () => setAuthenticated(false)
     const handlePick = () => setAuthenticated(true)
     
-
+    const gardens = ['Garden 1', 'My Walnut']; 
+    const [selectedValue, setSelectedValue] = useState(gardens[0]);
 
     return (
+      <LinearGradient colors={['#89C6A7', '#89C6A7']} style={{height: '100%'}}>
+        <View style={styles.container}>
+          <Text style={styles.text}>add photo</Text>
+          {isAuthenticated ? (
+            <UnPick pick={handlePick} />
+          ) : (
+            <Pick unPick={handleUnPick} />
+          )}
 
-        <LinearGradient
-            colors={["#89C6A7", "#89C6A7"]}
-            style={{ height: "100%" }}
-        >
-            <View style={styles.container}>
-
-                <Text style={styles.text}>add photo</Text>
-                {
-                    isAuthenticated?<Pick unPick={handleUnPick} /> : <UnPick pick={handlePick}/>
-                }
-
-                <Text style={styles.t4}>
-                    Select a garden
-                </Text>
-
-            </View>
-
-        </LinearGradient>
-
-    )
+          <Text style={styles.t4}>Select a garden</Text>
+        
+        
+          <Picker
+            selectedValue={selectedValue}
+            style={{height: 30, width: 200, backgroundColor: '#fff'}}
+            onValueChange={(itemValue, itemIndex) =>
+              setSelectedValue(itemValue)
+            }>
+            {gardens.map(garden => (
+              <Picker.Item key={garden} label={garden} value={garden} />
+            ))}
+          </Picker>
+        </View>
+      </LinearGradient>
+    );
 }
 
 export default Camera
